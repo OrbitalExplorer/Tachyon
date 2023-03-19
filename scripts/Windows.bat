4de4081671e90900fcb9625410ea40e0bb4bb28d
lalamapaka <obe.willaert@gmail.com>
2023-03-19 11:54:08 +0100

    Update Windows.bat

--- scripts/Windows.bat
+++ scripts/Windows.bat
@@ -1,19 +1,20 @@
-@echo off 
-:start 
-java -Xmx3G -Xms3G -jar quilt-server-launch.jar nogui 
-set /a "t=10" 
-:loop 
-set /a "t-=1" 
-if "" == "0" goto timedout 
-cls 
-choice /T 1 /C sc /N /D s /M "Restart in  seconds, press C to cancel: " 
-if not "0" == "1" goto cancelled  
-goto :loop 
-:cancelled 
-echo You cancelled shutdown 
-goto :end 
-:timedout 
-echo Restarting down 
-goto :start 
-:end 
-pause 
+@echo off
+:loop
+java -Xmx3G -Xms3G -jar quilt-server-launch.jar nogui
+set /a t=10
+:countdown
+cls
+echo Restart in %t% seconds, press C to cancel:
+choice /c YC /n /t 1 /d Y > nul
+if errorlevel 2 (
+    echo You cancelled shutdown
+    exit 0
+)
+set /a t-=1
+if %t% gtr 0 goto countdown
+
+echo Restarting ...
+
+goto loop
+
+pause
